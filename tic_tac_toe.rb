cd958825a7a17e032e70ba6571e2cce43b243e23
dipsthedestroya <dipupurecha01@gmail.com>
2021-10-09 20:42:47 +0400

    cleaned code and fixed bugs

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
+
 require 'pry'
 class Game
-
   def initialize(maps = [0, 1, 2, 3, 4, 5, 6, 7, 8])
     @@maps = maps
     @@gameover = false
-    @@winning_positions=[[0, 4, 8], [0,1,2], [0,3,6], [2,4,6], [2,5,8], [6,7,8],[1, 4, 7], [3, 4, 5]]
+    @@winning_positions = [[0, 4, 8], [0, 1, 2], [0, 3, 6], [2, 4, 6], [2, 5, 8], [6, 7, 8], [1, 4, 7], [3, 4, 5]]
     @@count = 0
-    print_screen()
+    print_screen
   end
 
   def self.gameover
@@ -14,11 +15,11 @@ class Game
   end
 
   def self.count=(count)
-    @@count=count
+    @@count = count
   end
 
   def self.gameover=(gameover)
-    @@gameover=gameover
+    @@gameover = gameover
   end
 
   def self.maps
@@ -33,9 +34,7 @@ class Game
     @@winning_positions
   end
 
-
-  def print_screen()
-
+  def print_screen
     puts "\n       |       |\n"
     puts "   #{@@maps[0]}   |   #{@@maps[1]}   |   #{@@maps[2]}"
     puts "_______|_______|_______\n"
@@ -45,96 +44,78 @@ class Game
     puts "       |       |           \n"
     puts "   #{@@maps[6]}   |   #{@@maps[7]}   |   #{@@maps[8]}"
     puts "       |       |           \n\n"
-    end
+  end
 end
-
 
 class Player < Game
   attr_accessor :player, :position, :player_won
-  def initialize()
+
+  def initialize
     super
     @position = []
-    @player=""
+    @player = ''
   end
 
-  def print_screen()
-    super
-  end
-
-  def set_character(taken_character=nil)
-    if taken_character == nil
-      puts "Input any character you like\n" 
-      @player = gets.chomp
-      @player_won = false
+  def set_character(taken_character = nil)
+    if taken_character.nil?
+      puts "Input any character you like\n"
+      @player = gets.chomp[0]
     else
       loop do
-        puts "Input any character you like except #{taken_character}\n" 
-        @player = gets.chomp
-        unless @player == taken_character
-          break
-        end
+        puts "Input any character you like except #{taken_character}\n"
+        @player = gets.chomp[0]
+        break unless @player == taken_character
       end
     end
   end
-
-
 end
 
-player1=Player.new()
-player2=Player.new()
-Game.maps
-puts "Player 1 >>"
-player1.set_character()
-puts "Player 2 >>"
+def end_game(player)
+  Game.gameover = true
+  player.player_won = true
+end
+
+def update_player_position(player, position)
+  Game.maps[position.to_i] = player.player
+  player.position.push(position.to_i)
+end
+
+def check_winner(player)
+  Game.winning_positions.each do |element|
+    if element.sort.intersection(player.position).length == 3
+      end_game(player)
+      Game.gameover = true
+      return true
+    end
+  end
+end
+
+def update_player(player, position, turn)
+  update_player_position(player, position)
+  player.print_screen
+  puts "Player #{turn} won the game!" if check_winner(player) == true
+end
+
+player1 = Player.new
+player2 = Player.new
+puts 'Player 1 >>'
+player1.set_character
+puts 'Player 2 >>'
 player2.set_character(player1.player)
-
-while Game.gameover == false 
-  Game.count % 2 == 0 ? turn = 1 : turn = 2
-  player1.print_screen()
-
-  loop do 
-
+player1.print_screen
+while Game.gameover == false && Game.count != 9
+  turn = Game.count.even? ? 1 : 2
+  loop do
     puts "Player #{turn}>> Please enter the position where you would like your marker"
     position = gets.chomp
-    if Game.maps.include?(position.to_i)
-      if turn == 1 
-        Game.maps[position.to_i] = player1.player
-        player1.position.push(position.to_i)
-        # byebug
-        Game.winning_positions.each do |element| 
-          if element.sort.intersection(player1.position).length==3
-            Game.gameover=true;
-            player1.player_won=true
-            puts "Player 1 won the game!"
-            player1.print_screen
-            break
-          end
-        end
-      else  
-        Game.maps[position.to_i] = player2.player
-        player2.position.push(position.to_i)
-        # byebug
-        Game.winning_positions.each do |element|
-          if element.sort.intersection(player2.position).length == 3
-            Game.gameover = true;
-            player2.player_won = true
-            puts "Player 2 won the game!"
-            player2.print_screen
-            break
-          end
-        end
-      end
-      puts Game.count
-      Game.count = Game.count + 1
-      break
-    end
+    p position 
+    next unless Game.maps.include?(position.to_i)
+
+    turn == 1 ? update_player(player1, position, turn) : update_player(player2, position, turn)
+    Game.count = Game.count + 1
+    p Game.count
+    Game.gameover = true if Game.count == 9
+    break
   end
-
 end
-        
-
-
-
-    
-
-
+puts "Draw" if Game.count == 9
