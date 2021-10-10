f68e2131542ae6471858482743422488c7dc2844
dipsthedestroya <dipupurecha01@gmail.com>
2021-10-10 13:55:13 +0400

    Cleaned code

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -11,6 +11,34 @@ class Game
     print_screen
   end
 
+  def play_game()
+    player1 = Player.new
+    player2 = Player.new
+    puts 'Player 1 >>'
+    player1.set_character
+    puts 'Player 2 >>'
+    player2.set_character(player1.player)
+    print_screen
+    game_loop(player1, player2)
+    puts 'Draw' if @count == 9
+  end
+
+  def game_loop(player1, player2)
+    while @gameover == false && @count != 9
+      turn = @count.even? ? 1 : 2
+      loop do
+        puts "Player #{turn}>> Please enter the position where you would like your marker"
+        position = gets.chomp
+        next unless @maps.include?(position.to_i)
+
+        turn == 1 ? update_player(player1, position, turn) : update_player(player2, position, turn)
+        @count += 1
+        @gameover = true if @count == 9
+        break
+      end
+    end
+  end
+
   def print_screen
     puts "\n       |       |\n"
     puts "   #{@maps[0]}   |   #{@maps[1]}   |   #{@maps[2]}"
@@ -22,26 +50,27 @@ class Game
     puts "   #{@maps[6]}   |   #{@maps[7]}   |   #{@maps[8]}"
     puts "       |       |           \n\n"
   end
+
   def end_game(player)
     @gameover = true
     player.player_won = true
   end
-  
+
   def update_player_position(player, position)
     @maps[position.to_i] = player.player
     player.position.push(position.to_i)
   end
-  
+
   def check_winner(player)
     @winning_positions.each do |element|
-      if element.sort.intersection(player.position).length == 3
+      next unless (element.sort & player.position).length == 3
         end_game(player)
         @gameover = true
         return true
       end
     end
   end
-  
+
   def update_player(player, position, turn)
     update_player_position(player, position)
     print_screen
@@ -49,7 +78,9 @@ class Game
   end
 end
 
+# class for player
 class Player
+
   attr_accessor :player, :position, :player_won
 
   def initialize
@@ -72,23 +103,4 @@ class Player
 end
 
 game = Game.new
-player1 = Player.new
-player2 = Player.new
-puts 'Player 1 >>'
-player1.set_character
-puts 'Player 2 >>'
-player2.set_character(player1.player)
-game.print_screen
-while game.gameover == false && game.count != 9
-  turn = game.count.even? ? 1 : 2
-  loop do
-    puts "Player #{turn}>> Please enter the position where you would like your marker"
-    position = gets.chomp
-    next unless game.maps.include?(position.to_i)
-    turn == 1 ? game.update_player(player1, position, turn) : game.update_player(player2, position, turn)
-    game.count = game.count + 1
-    game.gameover = true if game.count == 9
-    break
-  end
-end
-puts "Draw" if game.count == 9
+game.play_game()
